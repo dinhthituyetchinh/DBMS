1144714f2f316fc80901cb452aaa791f56c59616
dinhthituyetchinh <fengdinh@gmail.com>
2023-10-19 23:40:40 +0700

    updates 3

---
+++ Example/Example3.sql
@@ -0,0 +1,13 @@
+﻿
+-- HÀM UPPER => dùng để chuyển đổi một chuỗi ký tự thành chữ in hoa
+SELECT UPPER(N' Đinh Thị Tuyết Chinh') as 'IN HOA TÊN' 
+SELECT N'Họ và tên sinh viên' = UPPER(HOTEN) FROM SINHVIEN
+-- HÀM LOWER => dùng để chuyển đổi một chuỗi ký tự thành chữ thường
+SELECT LOWER(N'ĐINH THỊ TUYẾT CHINH') as 'IN THƯỜNG TÊN' 
+SELECT N'Họ và tên sinh viên' = LOWER(HOTEN) FROM SINHVIEN
+-- HÀM LEFT => trả về một chuỗi con được trích ra từ chuỗi nguồn bắt đầu từ vị trí bên trái
+PRINT LEFT('Đinh Thị Tuyết Chinh', 4)
+-- HÀM RIGHT => trả về một chuỗi con được trích ra từ chuỗi nguồn bắt đầu từ vị trí bên phải
+PRINT RIGHT('Đinh Thị Tuyết Chinh', 5)
+SELECT * FROM SINHVIEN
+ 
--- Example/ExampleSPs.sql
+++ Example/ExampleSPs.sql
@@ -53,11 +53,11 @@ EXEC CONG @a = 3, @b = 2
 		WHERE MASV = @MASV
 	GO
 
-	EXEC TT_SV
+	-- EXEC TT_SV
 
 --- Mã hoá thủ tục
 CREATE PROC ENCRYTION_MASV
-@MASV CHAR
+@MASV CHAR(10)
 WITH ENCRYPTION
 AS
 	BEGIN
@@ -68,8 +68,56 @@ AS
 GO
 sp_HelpText ENCRYTION_MASV
 
+--- Biên dịch thủ tục
+--CREATE PROC RECOMPILE
+--@MASV CHAR
+--WITH RECOMPILE
+--AS
+--	BEGIN
+--		SELECT *
+--		FROM SINHVIEN
+--		WHERE MASV = @MASV
+--	END
+--GO
 
 
-EXEC ENCRYTION_MASV @MASV = 'SV002'
+CREATE PROCEDURE sp_dosomething_sub
+@AdrID CHAR
+WITH RECOMPILE
+AS
+    SELECT TENKHOA FROM KHOA WHERE MAKHOA = @AdrID
+GO
 
-	SELECT * FROM SINHVIEN
+
+EXEC  sp_recompile sp_dosomething_sub @AdrID = 'QT'
+
+	SELECT * FROM KHOA
+
+--- Gọi hàm trong thủ tục	
+-- Định nghĩa một hàm
+CREATE FUNCTION MyFunction (@param1 INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @result INT
+    -- Thực hiện các công việc trong hàm và gán kết quả cho @result
+    SET @result = @param1 * 2
+    RETURN @result
+END
+
+
+-- Gọi hàm
+ DECLARE @PARAM INT
+ SET @PARAM = dbo.MyFunction(5)
+ PRINT @PARAM
+-- Định nghĩa một thủ tục lưu trữ
+CREATE PROCEDURE MyProcedure
+AS
+BEGIN
+    DECLARE @output INT
+    -- Gọi hàm MyFunction và lưu kết quả vào @output
+    SET @output = dbo.MyFunction(5)
+    -- Sử dụng kết quả trong thủ tục lưu trữ
+    PRINT N'Kết quả từ hàm là: ' + CAST(@output AS VARCHAR)
+END
+EXEC MyProcedure
